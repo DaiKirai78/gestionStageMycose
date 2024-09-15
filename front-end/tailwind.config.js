b503b42e5c951df483d91486f8149dd1b446fec3
JasonJody <jasonjody2005@gmail.com>
2024-09-15 16:01:06 -0400

    React avec Vite et Tailwind

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+
+import withMT from "@material-tailwind/react/utils/withMT";
+
+export default withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      "orange": "#FB7813",
+      "orange-light": "#FFF8F2",
+      "gris": "#A7A7A7"
+    },
+    extend: {
+      padding: {
+        '10pct': '10%',
+        '20pct': '20%',
+        '30pct': '30%',
+      },
+    },
+  },
+  plugins: [],
+})
+
