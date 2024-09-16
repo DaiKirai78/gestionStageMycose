96188e2f52d3e8d4ba7535cd9bfef67e3a08a0a5
Samuel <samleveille8@gmail.com>
2024-09-16 01:44:59 -0400

    Ajout de postgres dans le pom.xml

--- src/main/java/com/projet/mycose/MycoseApplication.java
+++ src/main/java/com/projet/mycose/MycoseApplication.java
@@ -1,14 +1,17 @@
 package com.projet.mycose;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class MycoseApplication {
+@SpringBootApplication
+public class MycoseApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MycoseApplication.class, args);
 		System.out.println("Hello world!");
 	}
+
+	@Override
+	public void run(String... args) { }
 }
