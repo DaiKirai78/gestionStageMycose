eec6133c65eb5523a175168d81c4271d4869fc7b
JasonJody <jasonjody2005@gmail.com>
2024-09-16 19:23:10 -0400

    Setup traduction page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,36 @@
 import './App.css'
 
+/*
+Doc traduction:
+import { useTranslation } from "react-i18next"
+
+const { t, i18n } = useTranslation()
+
+Pour aller chercher du texte traduit:
+t("key")
+
+Si une clé existe pas, le deuxième paramètre sera montré automatiquement
+t("CleQuiExistePas", "Automatique")
+
+Pour changer la langue :
+i18n.changeLanguage(langue)
+
+Récupérer la langue courrante
+i18n.resolvedLanguage
+
+
+Exemple de hashmap pour un language switcher:
+const langues = {
+    en: { nativeName: "English" },
+    fr: { nativeName: "Français" }
+  }
+*/
+
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        Hello World!
       </h1>
     </>
   )
