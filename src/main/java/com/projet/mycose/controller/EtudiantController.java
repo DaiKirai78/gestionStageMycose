09040c28ef578a509c6141f3927e5db1d6b14c46
Samuel <samleveille8@gmail.com>
2024-09-16 00:20:01 -0400

    Ajout du Controller Etudiant et de la méthode CréationCompte

---
+++ src/main/java/com/projet/mycose/controller/EtudiantController.java
@@ -0,0 +1,25 @@
+package com.projet.mycose.controller;
+
+import com.projet.mycose.service.EtudiantService;
+import com.projet.mycose.service.dto.EtudiantDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins="http://localhost:3000")
+@RequestMapping("api/etudiant")
+public class EtudiantController {
+    private final EtudiantService etudiantService;
+
+    public EtudiantController(EtudiantService etudiantService) {
+        this.etudiantService = etudiantService;
+    }
+
+    @PostMapping("/nouveau-compte")
+    public ResponseEntity<HttpStatus> CreationCompte(@RequestBody EtudiantDTO etudiantDTO) {
+        EtudiantDTO etudiantResultat = etudiantService.CreationDeCompte(etudiantDTO);
+        return etudiantResultat != null ? ResponseEntity.status(HttpStatus.CREATED).build() : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+}
