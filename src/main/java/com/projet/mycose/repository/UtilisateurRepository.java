0df368528fa59db7c0b45af4ee647eaf174a74cc
Samuel <samleveille8@gmail.com>
2024-09-15 23:37:01 -0400

    Ajout du service pour étudiant ainsi que la méthode CreationDeCompte

--- src/main/java/com/projet/mycose/repository/UtilisateurRepository.java
+++ src/main/java/com/projet/mycose/repository/UtilisateurRepository.java
@@ -2,6 +2,8 @@ package com.projet.mycose.repository;
 
 import com.projet.mycose.modele.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
 }
--- src/main/java/com/projet/mycose/service/EtudiantService.java
+++ src/main/java/com/projet/mycose/service/EtudiantService.java
@@ -1,4 +1,24 @@
 package com.projet.mycose.service;
 
+import com.projet.mycose.repository.EtudiantRepository;
+import com.projet.mycose.service.dto.EtudiantDTO;
+import org.springframework.stereotype.Service;
+
+import static com.projet.mycose.service.dto.EtudiantDTO.toDTO;
+import static com.projet.mycose.service.dto.EtudiantDTO.toEntity;
+
+@Service
 public class EtudiantService {
+
+    private final EtudiantRepository etudiantRepository;
+
+    public EtudiantService(EtudiantRepository etudiantRepository) {
+        this.etudiantRepository = etudiantRepository;
+    }
+
+    public EtudiantDTO CreationDeCompte(EtudiantDTO etudiantInformations) {
+        if (etudiantInformations != null)
+            return toDTO(etudiantRepository.save(toEntity(etudiantInformations)));
+        throw new IllegalArgumentException("Les informations de l'étudiant ne peuvent pas être nulles.");
+    }
 }
