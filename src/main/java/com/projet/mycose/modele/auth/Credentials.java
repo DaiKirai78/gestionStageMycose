95dc03b8b1fb9df79164e99c4fd3fe69f89848e3
Samuel <samleveille8@gmail.com>
2024-09-16 13:06:49 -0400

    Ajout du package auth, de la classe Credentials et de l'énumération Role

---
+++ src/main/java/com/projet/mycose/modele/auth/Credentials.java
@@ -0,0 +1,37 @@
+package com.projet.mycose.modele.auth;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Embeddable
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public final class Credentials implements UserDetails {
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(role.name()));
+    }
