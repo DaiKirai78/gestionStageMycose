670d0157ceb6fe3835aae6f027e7356db79b2563
JasonJody <jasonjody2005@gmail.com>
2024-09-17 16:38:07 -0400

    Ajout divider

---
+++ front-end/src/components/divider.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Divider = ({texte}) => {
+    return (
+        <div className='flex align-middle my-4'>
+                <hr className='w-full h-[2px] bg-gris m-auto mr-2' />
+                <p className='w-auto text-center text-nowrap text-gris text-xs mx-1'>{texte}</p>
+                <hr className='w-full h-[2px] bg-gris m-auto ml-2' />
+              </div>
+    );
+};
+
+export default Divider;
