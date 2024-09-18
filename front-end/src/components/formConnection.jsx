a471bb9547ce13fb17ade8089c97274a02191e26
JasonJody <jasonjody2005@gmail.com>
2024-09-17 20:09:26 -0400

    Début envoie formulaire au BE

--- front-end/src/components/formConnection.jsx
+++ front-end/src/components/formConnection.jsx
@@ -6,7 +6,7 @@ import InputErrorMessage from './inputErrorMesssage';
 const FormConnection = () => {
 
     const validEmail = new RegExp(
-        '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$'
+        '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z]{2,}$'
     );
 
     const validPassword = new RegExp(
@@ -19,26 +19,49 @@ const FormConnection = () => {
     const [password, setPassword] = useState()
     const [errorKeyPassword, setErrorKeyPassword] = useState("")
     
+    function onLogin(e) {
+        e.preventDefault()
+
+        if(!verifierInputs()) {
+            return
+        }
+        
+        sendLogin({email, password})
+
+        setEmail('')
+        setPassword('')
+    }
+
+    async function sendLogin(loginInfo) {
+        //Url temporaire
+        const res = await fetch(`urlLoginController/api?email=${loginInfo["email"]}&pass=${loginInfo["email"]}`);
+
+        //Vérification - actions => code erreurs/succès
+    }
+
     function verifierInputs() {
-        verifierCourriel()
-        verifierMotDePasse()
+        return verifierCourriel() && verifierMotDePasse()
     }
 
     function verifierCourriel() {
         if (!validEmail.test(email)) {
             setErrorKeyEmail("errorMessageEmail")
+            return false
         }
-    }
-
-    function changeEmailValue(e) {
-        setEmail(e.target.value)
-        setErrorKeyEmail("")
+        return true
     }
 
     function verifierMotDePasse() {
         if (!validPassword.test(password)) {
             setErrorKeyPassword("errorMessagePassword")
+            return false
         }
+        return true
+    }
+
+    function changeEmailValue(e) {
+        setEmail(e.target.value)
+        setErrorKeyEmail("")
     }
 
     function changePasswordValue(e) {
@@ -48,27 +71,27 @@ const FormConnection = () => {
 
     return (
         <div className='flex flex-col px-10'>
-            <div className='flex flex-col sm:gap-5 gap-3'>
+            <form method="get" className='flex flex-col'>
                 <div>
-                    <div className="w-full">
+                    <div className="w-full sm:mb-5 mb-3">
                         <Input label="Courriel" color='black' size='lg' 
                         onChange={(e) => {changeEmailValue(e)}}
                         type='email'
                         />
+                        <InputErrorMessage messageKey={errorKeyEmail}/>
                     </div>
-                    <InputErrorMessage messageKey={errorKeyEmail}/>
                 </div>
                 <div>
                     <div className="w-full">
                         <Input label="Mot de passe" color='black' size='lg'
                         onChange={(e) => {changePasswordValue(e)}}
                         type='password'/>
+                        <InputErrorMessage messageKey={errorKeyPassword}/>
                     </div>
-                    <InputErrorMessage messageKey={errorKeyPassword}/>
                 </div>
-            </div>
-            <a href="#" className='text-orange mt-2 mb-4 self-end text-xs'>Mot de passe oublié ?</a>
-            <button className='border p-2 border-black rounded-[7px]' onClick={verifierInputs}>Connexion</button>
+                <a href="#" className='text-orange mt-2 mb-4 self-end text-xs'>Mot de passe oublié ?</a>
+                <button className='border p-2 border-black rounded-[7px]' onClick={onLogin}>Connexion</button>
+            </form>
             <Divider texte="Nouveau sur Mycose ?"/>
             <button className='p-2 border border-black bg-black rounded-[7px] text-white'>S'inscrire</button>
         </div>
