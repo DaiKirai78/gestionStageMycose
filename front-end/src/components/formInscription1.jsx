083451bbc8a9b406e671e2ce33729d0cb1e490e9
Vicente Cabezas <1977259@claurendeau.qc.ca>
2024-09-19 17:52:19 -0400

    Ajout validation form inscription 1

--- front-end/src/components/formInscription1.jsx
+++ front-end/src/components/formInscription1.jsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
 import {Input} from '@material-tailwind/react';
+import Divider from './divider';
+import InputErrorMessage from './inputErrorMesssage';
+import { useState } from "react";
 
-const FormInscription1 = () => {
+function FormInscription1({prenom, nom, setPrenom, setNom}) {
+
+    const [errorKeyPrenom, setErrorKeyPrenom] = useState('');
+    const [errorKeyNom, setErrorKeyNom] = useState('');
 
     // const valideEmail = new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z]{2,}$');
-    const valideName = new RegExp("[a-zA-Z -'éÉàÀ]");
+    const valideName = new RegExp(String.raw`\D`);
     //const validePassword = new RegExp("[a-zA-Z0-9$&+,:;=?@#|'<>.^*()%!-]{8,}");
-
-    const [prenom, setPrenom] = useState('');
-    const [nom, setNom] = useState('');
-    const [nomEntreprise, setNomEntreprise] = useState('');
 
     function onNext(e) {
         e.preventDefault();
@@ -17,23 +18,45 @@ const FormInscription1 = () => {
         if(!validerChamps()) {
             console.log("Erreur inscription un")
             return;
-        }
+        }        
     }
 
     function validerChamps() {
+        const prenomValide = verifierPrenom();
+        const nomValide = verifierNom();
+
+        console.log(prenom);
+        console.log(prenomValide);
+        console.log(nom);
+        console.log(nomValide);
         
+        return prenomValide && nomValide;
+    }
+
+    function verifierPrenom() {
+        if(!valideName.test(prenom)) {
+            setErrorKeyPrenom("Le prénom est invalide");
+            return false
+        }
+        return true
+    }
+
+    function verifierNom() {
+        if(!valideName.test(nom)) {
+            setErrorKeyNom("Le nom est invalide");
+            return false
+        }
+        return true
     }
 
     function changePrenomValue(e) {
         setPrenom(e.target.value);
+        setErrorKeyPrenom("");
     }
 
     function changeNomValue(e) {
         setNom(e.target.value);
-    }
-
-    function changeNomEntrepriseValue(e) {
-        setNomEntreprise(e.target.value);
+        setErrorKeyNom("");
     }
 
     return (
@@ -43,30 +66,27 @@ const FormInscription1 = () => {
                     <div>
                         <div className="w-full">
                             <Input label="Prénom" color='black' size='lg' 
-                            onChange={(e) => {changeEmailValue(e);}}
+                            onChange={(e) => {changePrenomValue(e);}}
                             type='text'
                             />
+                            <InputErrorMessage messageKey={errorKeyPrenom}/>
                         </div>
                     </div>
                     <div>
                         <div className="w-full">
                             <Input label="Nom" color='black' size='lg'
-                            onChange={(e) => {changePasswordValue(e);}}
+                            onChange={(e) => {changeNomValue(e);}}
                             type='text'/>
-                        </div>
-                    </div>
-                    <div>
-                        <div className="w-full">
-                            <Input label="Nom de l'entreprise" color='black' size='lg'
-                            onChange={(e) => {changeTelephoneValue(e);}}
-                            type='tel'/>
+                            <InputErrorMessage messageKey={errorKeyNom}/>
                         </div>
                     </div>
                     <button className='border p-2 border-black rounded-[7px]' onClick={onNext}>Suivant</button>
                 </form>
-                <p>Déjà un compte?</p>
+                <p className="text-center mt-3 text-sm text-gray-800">1/3</p>
+                <Divider texte={"Déjà un compte ?"}/>
                 <button className='p-2 border border-black bg-black rounded-[7px] text-white'>Connexion</button>
-            </div>	
+               
+            </div>
         </>
     );
 }
--- front-end/src/pages/inscriptionPage.jsx
+++ front-end/src/pages/inscriptionPage.jsx
@@ -1,10 +1,18 @@
 import FormInscriptionEtudiant from '../components/formInscription2.jsx'
 import FormInscription1 from '../components/formInscription1.jsx';
+import FormContainer from '../components/formContainer.jsx';
+import { useState } from "react";
 
 const InscriptionPage = () => {
+    const [prenom, setPrenom] = useState('');
+    const [nom, setNom] = useState('');
+
     return (
         <>
-            <FormInscription1/>
+        <FormContainer>
+            <FormInscription1 prenom={prenom} nom={nom} setNom={setNom} setPrenom={setPrenom}/>
+        </FormContainer>
+            
             {/* <FormInscriptionEtudiant/> */}
         </>
     );
