ac78ab5c51bd21068c432b6808fb1c6b54077509
JasonJody <jasonjody2005@gmail.com>
2024-09-21 22:12:27 -0400

    i18n, gestion erreur fetch

--- front-end/src/components/formConnection.jsx
+++ front-end/src/components/formConnection.jsx
@@ -2,8 +2,14 @@ import React, { useState } from 'react';
 import Divider from './divider';
 import { Input } from '@material-tailwind/react';
 import InputErrorMessage from './inputErrorMesssage';
+import { useTranslation } from "react-i18next"
 
 const FormConnection = () => {
+
+    const  ERROR_CODE_UNAUTHORIZED = 401
+    const  REPSONSE_CODE_OK = 200
+
+    const { t } = useTranslation()
 
     const validEmail = new RegExp(
         '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z]{2,}$'
@@ -18,6 +24,9 @@ const FormConnection = () => {
 
     const [password, setPassword] = useState("");
     const [errorKeyPassword, setErrorKeyPassword] = useState("");
+
+    const [isFetching, setIsFetching] = useState(false)
+    const [errorKeyResponse, setErrorKeyResponse] = useState(false)
     
     function onLogin(e) {
         e.preventDefault();
@@ -27,21 +36,43 @@ const FormConnection = () => {
         }
         
         sendLoginInfo({email, password});
-
-        setEmail('');
-        setPassword('');
     }
 
     async function sendLoginInfo(loginInfo) {
-        //Url temporaire
-        const res = await fetch(`urlLoginController/api?email=${loginInfo["email"]}&pass=${loginInfo["email"]}`);
+        setIsFetching(true)
+        try {
+            const res = await fetch("http://localhost:8080/utilisateur/login",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        "courriel": "robyking@gmail.com",
+                        "motDePasse": "$2a$10$puJ.SglyQgU3SwtZacWxJuWTflHBW5ZieT9oQ5d39mn377T8PLhL2"
+                    })
+                }
+            );
 
-        //Vérification - actions => code erreurs/succès
+            if (res.status === REPSONSE_CODE_OK) {
+                console.log("LETS GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+                
+            } else if (res.status === ERROR_CODE_UNAUTHORIZED) {
+                setErrorKeyResponse("wrongEmailOrPassword")
+            } else {
+                setErrorKeyResponse("errorOccurredNotCode")
+            }
+
+        } catch (e) {
+            setErrorKeyResponse("errorOccurredNotCode")
+        }
+
+        setIsFetching(false)
     }
 
     function verifierInputs() {
-        const emailValide = verifierCourriel()
-        const passwordValide = verifierMotDePasse()
+        const emailValide = verifierCourriel();
+        const passwordValide = verifierMotDePasse();
         
         return emailValide && passwordValide;
     }
@@ -72,30 +103,47 @@ const FormConnection = () => {
         setErrorKeyPassword("");
     }
 
+    function renderMessageErreur() {
+        return(
+            <div className="text-center mb-5 rounded bg-red-200 py-3 bg-opacity-30">
+                <InputErrorMessage messageKey={errorKeyResponse}/>
+            </div>
+        );
+    }
+
     return (
+
         <div className='flex flex-col px-10'>
+            {(errorKeyResponse != "") ? renderMessageErreur() : null}
             <form method="get" className='flex flex-col sm:gap-5 gap-3'>
                 <div>
                     <div className="w-full">
-                        <Input label="Courriel" color='black' size='lg' 
+                        <Input label={t("email")} color='black' size='lg' 
                         onChange={(e) => {changeEmailValue(e);}}
                         type='email'
+                        autoComplete='on'
+                        value={email}
+                        error={errorKeyEmail.length > 0}
                         />
                         <InputErrorMessage messageKey={errorKeyEmail}/>
                     </div>
                 </div>
                 <div>
                     <div className="w-full">
-                        <Input label="Mot de passe" color='black' size='lg'
+                        <Input label={t("password")} color='black' size='lg'
                         onChange={(e) => {changePasswordValue(e);}}
-                        type='password'/>
+                        type='password'
+                        autoComplete='on'
+                        value={password}
+                        error={errorKeyPassword.length > 0}
+                        />
                         <InputErrorMessage messageKey={errorKeyPassword}/>
                     </div>
                 </div>
-                <button className='border p-2 border-black rounded-[7px]' onClick={onLogin}>Connexion</button>
+                <button className='border p-2 border-black rounded-[7px] hover:shadow-md' onClick={onLogin} disabled={isFetching}>{isFetching ? t("loading") : t("login")}</button>
             </form>
-            <Divider texte="Nouveau sur Mycose ?"/>
-            <button className='p-2 border border-black bg-black rounded-[7px] text-white'>S'inscrire</button>
+            <Divider translateKey="newOnMycose"/>
+            <button className='p-2 border border-black bg-black rounded-[7px] text-white hover:shadow-lg hover:bg-gray-900' disabled={isFetching}>{t("signin")}</button>
         </div>
     );
 };
