f347b2c9592b1483a4c67c7457b1bfcefaba39e3
Samuel <samleveille8@gmail.com>
2024-09-24 00:16:59 -0400

    Ajout des tests pour le controller getMe de UtilisateurController

--- src/test/java/com/projet/mycose/controller/UtilisateurControllerTest.java
+++ src/test/java/com/projet/mycose/controller/UtilisateurControllerTest.java
@@ -1,9 +1,12 @@
 package com.projet.mycose.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.projet.mycose.modele.auth.Role;
 import com.projet.mycose.service.EtudiantService;
 import com.projet.mycose.service.UtilisateurService;
+import com.projet.mycose.service.dto.EtudiantDTO;
 import com.projet.mycose.service.dto.LoginDTO;
+import com.projet.mycose.service.dto.UtilisateurDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -12,12 +15,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UtilisateurController.class)
 public class UtilisateurControllerTest {
@@ -118,6 +122,44 @@ public class UtilisateurControllerTest {
                         .with(csrf())
                         .with(user("karim").roles("ETUDIANT")))
                 .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void testGetMe_Success() throws Exception {
+        String token = "Bearer valid_token";
+        EtudiantDTO utilisateurDTO = new EtudiantDTO(1L, "Karim", "Mihoubi", "mihoubi@gmail.com", "438-508-2345", Role.ETUDIANT);
+
+        when(utilisateurService.getMe(anyString())).thenReturn(utilisateurDTO);
+
+        mockMvc.perform(post("/utilisateur/me")
+                        .header("Authorization", token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                .with(csrf())
+                .with(user("karim").roles("ETUDIANT")))
+                .andExpect(status().isAccepted())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.prenom").value("Karim"))
+                .andExpect(jsonPath("$.nom").value("Mihoubi"))
+                .andExpect(jsonPath("$.courriel").value("mihoubi@gmail.com"));
+
+        verify(utilisateurService).getMe("Bearer valid_token");
+    }
+
+    @Test
+    public void testGetMe_Failure() throws Exception {
+        String token = "Bearer invalid_token";
+
+        when(utilisateurService.getMe(anyString())).thenThrow(new RuntimeException("Erreur d'authentification"));
+
+        mockMvc.perform(post("/utilisateur/me")
+                        .header("Authorization", token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                .with(csrf())
+                .with(user("karim").roles("ETUDIANT")))
+                .andExpect(status().isInternalServerError());
+
+        verify(utilisateurService).getMe("Bearer invalid_token");
     }
 
 }
