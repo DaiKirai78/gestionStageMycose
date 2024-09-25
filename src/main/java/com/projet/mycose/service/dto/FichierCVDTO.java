18d4427c69a66bfb7ab43077339cdb0f9f247507
Elie-X <eliebouchergendron@gmail.com>
2024-09-25 18:24:32 -0400

    Ajout FichierCVDTO

---
+++ src/main/java/com/projet/mycose/service/dto/FichierCVDTO.java
@@ -0,0 +1,27 @@
+package com.projet.mycose.service.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class FichierCVDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Filename is required.")
+    @Size(max = 255, message = "Filename cannot exceed 255 characters.")
+    @Pattern(regexp = "^[\\p{L}0-9\\s,_.-]+\\.pdf$",
+            message = "Invalid filename format. Only PDF files are allowed.")
+    private String filename;
+
+    @NotBlank(message = "File data is required.")
+    @Pattern(regexp = "^[A-Za-z0-9+/=]+$",
+            message = "Invalid file data. Should be Base64 encoded.")
+    private String fileData;
+
+}
