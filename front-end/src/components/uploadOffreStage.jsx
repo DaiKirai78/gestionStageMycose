61d065a7f0ff90ea217f428983630e92175c3e73
Elie-X <eliebouchergendron@gmail.com>
2024-09-25 18:31:09 -0400

    Changement translation dans uploadOffreStage

--- front-end/src/components/uploadOffreStage.jsx
+++ front-end/src/components/uploadOffreStage.jsx
@@ -15,7 +15,7 @@ function UploadOffreStage() {
                 <h2 className="text-4xl font-bold mb-3 text-black">
                     {showFileUpload ? t("uploadFile") : t("fillForm")}
                 </h2>
-                <h3 className="mt-0 mb-4 text-xl">{showFileUpload ? t("uploadFilePDF") : t("fillFormInternship")}</h3>
+                <h3 className="mt-0 mb-4 text-xl">{showFileUpload ? t("uploadInternshipFilePDF") : t("fillFormInternship")}</h3>
                 <div className="flex justify-center space-x-4 w-full">
                     <button
                         className="px-4 py-2 rounded-lg bg-[#FFF8F2] border-2 border-black w-full shadow-md"
