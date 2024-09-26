dd50ad6991211055b5cbae04f8aecab15c6e1407
Samuel <samleveille8@gmail.com>
2024-09-25 21:53:08 -0400

    Ajout des tests pour le service Employeur

--- src/main/java/com/projet/mycose/controller/EmployeurController.java
+++ src/main/java/com/projet/mycose/controller/EmployeurController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
 @RequiredArgsConstructor
-@RequestMapping("employeur")
+@RequestMapping("entreprise")
 public class EmployeurController {
     private final EmployeurService employeurService;
 
