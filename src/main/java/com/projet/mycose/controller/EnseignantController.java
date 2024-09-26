43497b87fcde4da9155ce13390ce3858eec928a3
Samuel <samleveille8@gmail.com>
2024-09-25 23:59:00 -0400

    Ajout des controllers register pour un Enseignant

---
+++ src/main/java/com/projet/mycose/controller/EnseignantController.java
@@ -0,0 +1,38 @@
+package com.projet.mycose.controller;
+
+import com.projet.mycose.service.EnseignantService;
+import com.projet.mycose.service.dto.CourrielTelephoneDTO;
+import com.projet.mycose.service.dto.EnseignantDTO;
+import com.projet.mycose.service.dto.RegisterEtudiantEnseignantDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+@RequiredArgsConstructor
+@RequestMapping("enseignant")
+public class EnseignantController {
+    private final EnseignantService enseignantService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Object> CreationDeCompte(@Valid @RequestBody RegisterEtudiantEnseignantDTO nouveauCompteEnseignant) {
+        EnseignantDTO enseignantResultat = enseignantService.creationDeCompte(nouveauCompteEnseignant.getPrenom(),
+                nouveauCompteEnseignant.getNom(),
+                nouveauCompteEnseignant.getNumeroDeTelephone(),
+                nouveauCompteEnseignant.getCourriel(),
+                nouveauCompteEnseignant.getMotDePasse());
+        return enseignantResultat != null ? ResponseEntity.status(HttpStatus.CREATED).body(enseignantResultat) : ResponseEntity.status(HttpStatus.CONFLICT).body("L'enseignant existe déjà ou les credentials sont invalides");
+    }
+
+
+    @PostMapping("/register/check-for-conflict")
+    public ResponseEntity<Object> CreationDeCompte_CheckForConflict(@Valid @RequestBody CourrielTelephoneDTO courrielTelephoneDTO) {
+        if (enseignantService.credentialsDejaPris(courrielTelephoneDTO.getCourriel(), courrielTelephoneDTO.getTelephone()))
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("L'enseignant existe déjà ou les credentials sont invalides");
+        else
+            return ResponseEntity.status(HttpStatus.OK).body(courrielTelephoneDTO);
+    }
+}
