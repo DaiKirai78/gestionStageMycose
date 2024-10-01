c2c53a36926504dcd4819ea925518d2fbf22e39b
Samuel <samleveille8@gmail.com>
2024-09-30 20:36:31 -0400

    Ajout de la recherche de stage par titre ou entreprise

--- front-end/src/components/listeStage.jsx
+++ front-end/src/components/listeStage.jsx
@@ -7,6 +7,7 @@ const listeStage = () => {
     const [stagesFichier, setStagesFichier] = useState(null);
 
     const [rechercheStage, setRechercheStage] = useState("");
+    const [buttonDisabled, setButtonDisabled] = useState(true);
 
     let localhost = "http://localhost:8080/";
     let urlGetFormulaireStage = "/api/offres/get-all-forms";
@@ -20,37 +21,110 @@ const listeStage = () => {
     // }, []);
 
     useEffect(() => {
-        const fetchStages = async () => {
-            try {
-                const response = await axios.get("/stage.json");
-                setStagesFormulaire(response.data);
-            } catch (error) {
-                console.error("Erreur lors de la récupération des stages:", error);
-            }
-        };
-
         fetchStages();
-
     }, []);
 
-    function activerPossibiliteRecherche(e) {
-        const searchButton = document.getElementById("buttonRechercherStage");
+    const fetchStages = async () => {
+        try {
+            const response = await axios.get("/stage.json");
+            setStagesFormulaire(response.data);
+            return response.data;
+        } catch (error) {
+            console.error("Erreur lors de la récupération des stages:", error);
+        }
+    };
+
+    function activerDesactiverRecherche(e) {
         if (e.target.value.length > 0)
-            searchButton.disabled = false;
+            setButtonDisabled(false);
         else
-            searchButton.disabled = true
+            setButtonDisabled(true);
     }
+
+    function changeRechercheStage(e) {
+        setRechercheStage(e.target.value)
+    }
+
+    function rechercher(e) {
+        e.preventDefault();
+        let mapStages = stageEtNbreDeCorrespondances();
+
+        const mapStageSort = [...mapStages.entries()].sort((a, b) => b[1] - a[1]).map(([key, value]) => key);
+        const stagesDetails = fetchStages();
+        trierStages(stagesDetails, mapStageSort);
+    }
+
+    function stageEtNbreDeCorrespondances() {
+        let mapStages = new Map();
+        Object.entries(stagesFormulaire).forEach(([key, stage]) => {
+            const arrayofStringTitre = stage.titre.match(/\b[\wÀ-ÿ]+\b/gu);
+            const arrayofStringEntreprise = stage.entreprise.match(/\b[\wÀ-ÿ]+\b/gu);
+
+            rechercheStageParTitre(stage, mapStages, rechercheStage, arrayofStringTitre);
+            rechercheStageParEntreprise(stage, mapStages, rechercheStage, arrayofStringEntreprise);
+        });
+        return mapStages;
+    }
+
+    function rechercheStageParTitre(stage, mapStages, rechercheStage, arrayofStringTitre) {
+        let nbreDeMots = 0;
+        for (let i = 0; i < arrayofStringTitre.length; i++) {
+            if (rechercheStage.toLowerCase().trim().includes("programmeur") && arrayofStringTitre[i].toLowerCase() === "développeur")
+                nbreDeMots++;
+            if (rechercheStage.toLowerCase().trim().includes("développeur") && arrayofStringTitre[i].toLowerCase() === "programmeur")
+                nbreDeMots++
+            if (rechercheStage.toLowerCase().trim().includes((arrayofStringTitre[i].toLowerCase()))) {
+                nbreDeMots++;
+            }
+            mapStages.set(stage.titre, nbreDeMots);
+        }
+    }
+
+    function rechercheStageParEntreprise(stage, mapStages, rechercheStage, arrayofStringEntreprise) {
+        let nbreDeMots = 0;
+        for (let i = 0; i < arrayofStringEntreprise.length; i++) {
+            if (rechercheStage.toLowerCase().trim().includes((arrayofStringEntreprise[i].toLowerCase()))) {
+                nbreDeMots++;
+            }
+            mapStages.set(stage.entreprise, nbreDeMots);
+        }
+    }
+
+    function trierStages(stagesDetails, mapStageSort) {
+        stagesDetails.then(function (resultat) {
+            const titresAjoutes = new Set();
+
+            const resultatsFinals = mapStageSort
+                .map(titreOuEntreprise =>
+                    resultat.find(stage => {
+                        const estCorrespondant = stage.titre === titreOuEntreprise || stage.entreprise === titreOuEntreprise;
+                        if (estCorrespondant && !titresAjoutes.has(stage.titre)) {
+                            titresAjoutes.add(stage.titre);
+                            return stage;
+                        }
+                        return null;
+                    })
+                )
+                .filter(Boolean);
+
+            setStagesFormulaire(resultatsFinals);
+        });
+    }
+
 
     return (
         <div className="w-2/3 lg:w-1/2">
-            <h1 className="pt-12 text-3xl md:text-4xl text-pretty font-accueilTitreFont font-bold text-black">Explorez des stages dans des
+            <h1 className="pt-12 text-3xl md:text-4xl text-pretty font-accueilTitreFont font-bold text-black">Explorez
+                des stages dans des
                 entreprises variées</h1>
             <div className="flex flex-row">
                 <input type="search" placeholder="Chercher un stage" id="searchInput" onChange={(e) => {
-                    activerPossibiliteRecherche(e);
+                    activerDesactiverRecherche(e);
+                    changeRechercheStage(e);
                 }}
                        className="mt-6 mb-9 pt-3 pb-3 pl-14 text-md sm:text-xl rounded-l-3xl w-full outline-0"></input>
-                <button id="buttonRechercherStage" className="mt-6 mb-9 pt-3 pb-3 pl-12 sm:pl-16 md:pl-24 rounded-r-3xl" disabled={true}>
+                <button id="buttonRechercherStage" className="mt-6 mb-9 pt-3 pb-3 pl-12 sm:pl-16 md:pl-24 rounded-r-3xl"
+                        disabled={buttonDisabled} onClick={rechercher}>
                 </button>
             </div>
             <div>
@@ -59,13 +133,16 @@ const listeStage = () => {
                     stagesFormulaire && stagesFormulaire.length > 0 ?
                         stagesFormulaire.map((stage, index) => (
                             <>
-                                <div id="elementStage" className="flex flex-col w-full hover:cursor-pointer pb-3 pt-3 bg-orange-light transition-all duration-300 ease-in-out transform md:hover:-translate-y-1 md:hover:shadow-lg md:hover:rounded-t-xl">
+                                <div id="elementStage"
+                                     className="flex flex-col w-full hover:cursor-pointer pb-3 pt-3 bg-orange-light transition-all duration-300 ease-in-out transform md:hover:-translate-y-1 md:hover:shadow-lg md:hover:rounded-t-xl">
                                     <div>
                                         <h3 key={index} className="text-black text-xl pl-2">{stage.titre}</h3>
                                         <h4 className="text-lg pl-2">{stage.entreprise}</h4>
                                         <h4 className="pl-2">{stage.date}</h4>
                                     </div>
-                                    <button className="bg-orange text-white md:absolute md:bottom-2 lg:bottom-12 lg:top-4 md:right-2 px-4 md:px-8 py-2 rounded-2xl mt-3 lg:mt-0 w-1/2 md:w-1/3 lg:w-36 hover:bg-orange-dark shadow-md">Appliquer</button>
+                                    <button
+                                        className="bg-orange text-white md:absolute md:bottom-2 lg:bottom-12 lg:top-4 md:right-2 px-4 md:px-8 py-2 rounded-2xl mt-3 lg:mt-0 w-1/2 md:w-1/3 lg:w-36 hover:bg-orange-dark shadow-md">Appliquer
+                                    </button>
                                 </div>
                                 <hr className="bg-deep-orange-100"/>
                             </>
