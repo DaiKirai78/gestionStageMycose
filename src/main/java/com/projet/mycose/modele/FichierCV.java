807cf2f356bdb045da7c73ee11e8787785e7d660
JasonJody <jasonjody2005@gmail.com>
2024-10-02 19:16:23 -0400

    Ajout variables cv

--- src/main/java/com/projet/mycose/modele/FichierCV.java
+++ src/main/java/com/projet/mycose/modele/FichierCV.java
@@ -2,12 +2,21 @@ package com.projet.mycose.modele;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "fichiers_CV")
 @Getter
 @Setter
 public class FichierCV {
+
+    enum Status {
+        WAITING,
+        ACCEPTED,
+        REFUSED
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,4 +27,10 @@ public class FichierCV {
     @Column(name = "data", columnDefinition = "BYTEA")
     private byte[] data;
 
+    private Status status = Status.WAITING;
+
+    private String statusDescription;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
