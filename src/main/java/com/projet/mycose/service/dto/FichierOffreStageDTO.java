c42ad837c2806110df18424e07b8178f25ac0c2b
Vicente Cabezas <1977259@claurendeau.qc.ca>
2024-10-02 18:45:23 -0400

    Ajout - DTOs pour envoyer listes stages

--- src/main/java/com/projet/mycose/service/dto/FichierOffreStageDTO.java
+++ src/main/java/com/projet/mycose/service/dto/FichierOffreStageDTO.java
@@ -1,12 +1,16 @@
 package com.projet.mycose.service.dto;
 
+import com.projet.mycose.modele.FichierOffreStage;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
+@NoArgsConstructor
 @Getter
 @Setter
 public class FichierOffreStageDTO {
@@ -23,5 +27,20 @@ public class FichierOffreStageDTO {
     @Pattern(regexp = "^[A-Za-z0-9+/=]+$",
             message = "Invalid file data. Should be Base64 encoded.")
     private String fileData;
+
+    @Builder
+    public FichierOffreStageDTO(long id, String filename, String fileData) {
+        this.id = id;
+        this.filename = filename;
+        this.fileData = fileData;
+    }
+
+    public static FichierOffreStageDTO toDTO(FichierOffreStage fichierOffreStage) {
+        return FichierOffreStageDTO.builder()
+                .id(fichierOffreStage.getId())
+                .filename(fichierOffreStage.getFilename())
+                .fileData(fichierOffreStage.getFilename())
+                .build();
+    }
 
 }
--- src/main/java/com/projet/mycose/service/dto/FormulaireOffreStageDTO.java
+++ src/main/java/com/projet/mycose/service/dto/FormulaireOffreStageDTO.java
@@ -2,10 +2,14 @@ package com.projet.mycose.service.dto;
 
 
 
+import com.projet.mycose.modele.FormulaireOffreStage;
 import jakarta.validation.constraints.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
 @Getter
 @Setter
 public class FormulaireOffreStageDTO {
@@ -41,4 +45,31 @@ public class FormulaireOffreStageDTO {
     @NotBlank(message = "Description is required.")
     @Size(max = 500, message = "Description cannot exceed 500 characters.")
     private String description;
+
+    @Builder
+    public FormulaireOffreStageDTO(Long id, String entrepriseName, String employerName, String email, String website, String title, String location, String salary, String description) {
+        this.id = id;
+        this.entrepriseName = entrepriseName;
+        this.employerName = employerName;
+        this.email = email;
+        this.website = website;
+        this.title = title;
+        this.location = location;
+        this.salary = salary;
+        this.description = description;
+    }
+
+    public static FormulaireOffreStageDTO toDTO(FormulaireOffreStage formulaireOffreStage) {
+        return FormulaireOffreStageDTO.builder()
+                .id(formulaireOffreStage.getId())
+                .entrepriseName(formulaireOffreStage.getEntrepriseName())
+                .employerName(formulaireOffreStage.getEmployerName())
+                .email(formulaireOffreStage.getEmail())
+                .website(formulaireOffreStage.getWebsite())
+                .title(formulaireOffreStage.getTitle())
+                .location(formulaireOffreStage.getLocation())
+                .salary(formulaireOffreStage.getSalary())
+                .location(formulaireOffreStage.getLocation())
+                .build();
+    }
 }
