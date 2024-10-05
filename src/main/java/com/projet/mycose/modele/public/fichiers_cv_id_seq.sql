b5aec272488e97073b9305f61883dff1c912095f
Elie-X <eliebouchergendron@gmail.com>
2024-10-05 03:10:05 -0400

    Ajout tests pour ProgrammeController

--- src/main/java/com/projet/mycose/modele/public/fichiers_cv_id_seq.sql
+++
@@ -1,6 +0,0 @@
-create sequence fichiers_cv_id_seq;
-
-alter sequence fichiers_cv_id_seq owner to postgres;
-
-alter sequence fichiers_cv_id_seq owned by fichiers_cv.id;
-
--- src/main/java/com/projet/mycose/modele/public/offre_stage_id_seq.sql
+++
@@ -1,6 +0,0 @@
-create sequence offre_stage_id_seq;
-
-alter sequence offre_stage_id_seq owner to postgres;
-
-alter sequence offre_stage_id_seq owned by offre_stage.id;
-
