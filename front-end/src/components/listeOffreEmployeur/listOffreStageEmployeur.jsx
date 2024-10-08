07b59155e1045eff4098a7a31d273931f8fe1280
JasonJody <jasonjody2005@gmail.com>
2024-10-07 20:41:14 -0400

    Avancement sur UI

--- front-end/src/components/listeOffreEmployeur/listOffreStageEmployeur.jsx
+++ front-end/src/components/listeOffreEmployeur/listOffreStageEmployeur.jsx
@@ -1,26 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { BsArrowRight } from "react-icons/bs";
+import { BsX } from "react-icons/bs";
 
 
 const ListOffreStageEmployeur = ({data}) => {
 
-    const [activeOffer, setActiveOffer] = useState();
+    const [activeOffer, setActiveOffer] = useState(null);
 
-    useEffect(() => {
-        setActiveOffer(data[0]);        
+    useEffect(() => {        
+        if (!isMediumScreen()) {
+            setActiveOffer(data[0]);
+        }
     }, []);
+
+    function isMediumScreen() {        
+        return screen.width <= 720
+    }
 
     function linkCard(offre, index) {
         return (
             <button id="elementStage" 
                     key={index}
                     onClick={() => {setActiveOffer(offre)}}
-                    className="group relative px-6 flex items-center w-full cursor-pointer flex-col pb-3 pt-3 border border-deep-orange-50 rounded">
-                        <div className='absolute left-0 top-0 w-full h-full flex items-center justify-center opacity-0 group-hover:opacity-100 transition-all z-10'>
+                    className={`group relative px-6 flex items-center w-full  flex-col pb-3 pt-3 border rounded ${activeOffer === offre ? "border-deep-orange-200 cursor-default": "border-deep-orange-50 cursor-pointer"}`}>
+                        <div className={`absolute left-0 top-0 w-full h-full flex items-center justify-center opacity-0 group-hover:opacity-100 transition-all z-10 ${activeOffer === offre ? "group-hover:opacity-0" : ""}`}>
                         <div className='absolute left-0 top-0 w-full h-full bg-orange opacity-10'></div>
                             <p className='text-2xl text-orange relative ease-in-out flex items-center gap-1'>Détails <BsArrowRight /></p>
                         </div>
-                <div className='group-hover:opacity-20'>
+                <div className={activeOffer === offre ? "" : "group-hover:opacity-20"}>
                     <div className='flex flex-col gap-1 items-center justify-center'>
                         <h3
                             className="text-black text-xl text-center font-bold">{offre.title} - {offre.format}</h3>
@@ -60,8 +67,43 @@ const ListOffreStageEmployeur = ({data}) => {
                     })
                 }
             </div>
-            <div className='bg-orange w-full h-[calc(100vh-1rem)] sticky top-2 flex flex-col'>
-                {/* Contenu du deuxième div */}
+            <div className={`pb-16 sm:pt-0 bg-orange-light z-20 rounded border border-deep-orange-200 w-full md:h-[calc(100vh-1rem)] h-[90vh] fixed left-0 md:sticky md:top-2 flex flex-col md:transition-none transition-all ease-in-out ${activeOffer === null ? "bottom-[-90vh]" : "bottom-0"}`}>
+                <button className='absolute right-1/2 translate-x-[50%] bottom-2 bg-orange rounded-full p-2 text-white md:hidden'
+                    onClick={() => {setActiveOffer(null)}}>
+                    <BsX size={25}/>
+                </button>
+                { activeOffer &&
+                    <div className="p-6">
+                        <h2 className="text-2xl font-bold mb-2">{activeOffer.title}</h2>
+                        <p className="text-sm text-gray-600">Posté le : <span className="font-medium">{activeOffer.created_at}</span></p>
+                        <p className="text-sm text-gray-600">Mis à jour le : <span className="font-medium">{activeOffer.updated_at}</span></p>
+                        <h3 className="text-xl font-semibold mt-4">Détails de l'offre :</h3>
+                        { activeOffer.description &&
+                            <p className="mt-2"><strong>Description :</strong> {activeOffer.description}</p>
+                        }
+                        <p className="mt-2"><strong>Employeur :</strong> {activeOffer.employer_name}</p>
+                        <p className="mt-2"><strong>Entreprise :</strong> {activeOffer.entreprise_name}</p>
+                        { activeOffer.location &&
+                            <p className="mt-2"><strong>Location :</strong> {activeOffer.location}</p>
+                        }
+                        { activeOffer.salary &&
+                            <p className="mt-2"><strong>Salaire :</strong> {activeOffer.salary}</p>
+                        }
+                        <p className="mt-2"><strong>Statut :</strong> <span className={`font-semibold ${getColorOffreStatus(activeOffer.status)}`}>{activeOffer.status}</span></p>
+                        { activeOffer.website &&
+                            <p className="mt-2">
+                                <strong>Site Web :</strong> <a href={activeOffer.website} target="_blank" className="text-blue-500 underline cursor-pointer">{activeOffer.website}</a>
+                            </p>
+                        }
+                        { activeOffer.format === "file" &&
+                            <button
+                                className='bg-orange px-4 py-2 rounded text-white mt-3 cursor-pointer'
+                                onClick={() => console.log("LAWL")}
+                            >
+                                Voir pdf</button>
+                        }
+                    </div>
+                }
             </div>
         </div>
     );
