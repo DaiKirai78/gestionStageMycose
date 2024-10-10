be8324b50a55019246a34fb8598f5e6906c95981
Samuel <samleveille8@gmail.com>
2024-10-09 23:47:13 -0400

    Amélioration du style de la page et ajout du responsiveness lorsqu'un pdf est présent

--- front-end/src/components/appliquerStage.jsx
+++ front-end/src/components/appliquerStage.jsx
@@ -1,126 +1,84 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Worker, Viewer } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 const AppliquerStage = ({ vraiProps }) => {
-    const { title, entrepriseName, location, description, createdAt, pdfUrl } = vraiProps;
-    const [pdfFileUrl, setPdfFileUrl] = useState(null);
-    const [isPdfVisible, setIsPdfVisible] = useState(false);
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const { pdfUrl } = vraiProps;
+    const [showPDF, setShowPDF] = useState(false);
 
-    // Plugin pour le layout par défaut qui inclut les outils
     const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
-    useEffect(() => {
-        // Assurez-vous que pdfUrl est valide
-        if (pdfUrl) {
-            setPdfFileUrl(pdfUrl);
-        } else {
-            console.error('L\'URL du PDF est invalide.');
-        }
-    }, [pdfUrl]);
-
-    const showPdf = () => {
-        if (pdfFileUrl) {
-            setIsPdfVisible(true);
-        } else {
-            console.error('Le fichier PDF ne peut pas être affiché car l\'URL est manquante.');
-        }
-    };
-
-    const hidePdf = () => {
-        setIsPdfVisible(false);
-    };
-
-    const openModal = () => {
-        setIsModalOpen(true);
-    };
-
-    const closeModal = () => {
-        setIsModalOpen(false);
+    const togglePDF = () => {
+        setShowPDF(!showPDF);
     };
 
     return (
-        <div className="flex flex-col h-screen w-full bg-gray-100">
-            <div className="flex flex-col md:flex-row flex-1">
-                {/* Conteneur pour afficher le PDF (3/4 de la page) */}
-                <div className={`flex-1 md:w-3/4 ${isPdfVisible ? 'block' : 'hidden'} md:block`}>
-                    <div className="relative w-full h-full">
+        <div className="w-full 2xl:w-3/4 flex justify-center h-1/2 p-8">
+            <div className="w-full 2xl:w-5/6 h-auto lg:bg-gray-50 p-4 flex lg:flex-row flex-col rounded-2xl">
+
+                <div className={`lg:w-[90vh] w-full h-[70vh] border-2 border-gray-300 shadow-lg ${showPDF ? 'block' : 'hidden lg:block'}`}>
+                    <div className="w-full h-full overflow-hidden">
                         <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
-                            {pdfFileUrl ? (
-                                <Viewer
-                                    fileUrl={pdfFileUrl}
-                                    plugins={[defaultLayoutPluginInstance]}
-                                    className="h-full w-full"
-                                />
-                            ) : (
-                                <p>Le PDF n'est pas disponible.</p>
-                            )}
+                            <Viewer
+                                fileUrl={pdfUrl}
+                                plugins={[defaultLayoutPluginInstance]}
+                                className="w-full h-full"
+                                style={{ padding: 0, minHeight: '70vh' }}
+                            />
                         </Worker>
-                        {/* Bouton pour fermer le PDF, uniquement sur écrans plus petits */}
-                        {isPdfVisible && (
-                            <button
-                                className="absolute top-4 right-4 bg-red-600 text-white font-bold py-2 px-4 rounded hover:bg-red-500 transition duration-200"
-                                onClick={hidePdf}
-                            >
-                                Fermer
-                            </button>
-                        )}
                     </div>
+
+                    <button
+                        onClick={togglePDF}
+                        className={`mt-4 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 lg:hidden ${showPDF ? 'block' : 'hidden'}`}
+                    >
+                        Fermer PDF
+                    </button>
                 </div>
 
-                {/* Informations de stage (1/4 de la page) */}
-                <div className="flex-1 md:w-1/4 p-4 bg-white shadow-lg flex flex-col justify-center">
-                    <h1 className="text-2xl font-bold">{title}</h1>
-                    <h2 className="text-xl mt-2">{entrepriseName}</h2>
-                    <h3 className="text-lg mt-1">{location}</h3>
-                    <p className="mt-2">{description}</p>
-                    <p className="text-gray-500 mt-2">{createdAt}</p>
+                <div className={`lg:w-96 w-full bg-white shadow-md p-6 lg:ml-8 break-words rounded-2xl lg:rounded-none ${showPDF ? 'hidden' : 'block'}`}>
+                    <h2 className="text-2xl font-bold mb-4">{vraiProps.title || 'Offre de stage'}</h2>
+                    <p className="text-gray-700 text-xl">{vraiProps.entrepriseName || "Entreprise Inconnu"}</p>
+                    <p className="text-gray-500 mb-6">{vraiProps.location || 'Lieu par défaut'}</p>
+                    <p className="text-gray-600">{vraiProps.description || 'Pas de description'}</p>
+                    <p className="text-gray-500 mt-4">{vraiProps.createdAt || 'Date de création par défaut'}</p>
 
-                    {/* Bouton Appliquer */}
-                    <div className="flex justify-center mt-4">
-                        <button
-                            className="bg-green-600 text-white font-bold py-2 px-4 rounded hover:bg-green-500 transition duration-200"
-                            onClick={openModal}
-                        >
-                            Appliquer
-                        </button>
-                    </div>
+                    <button
+                        onClick={() => alert('Application confirmed!')}
+                        className="mt-6 bg-orange text-white px-4 py-2 rounded hover:bg-orange-dark"
+                    >
+                        Appliquer
+                    </button>
 
-                    {/* Bouton pour afficher le PDF (affiche uniquement sur les écrans sm et plus petits) */}
-                    <div className="flex justify-center mt-4 md:hidden">
-                        <button
-                            className="bg-blue-600 text-white font-bold py-2 px-4 rounded hover:bg-blue-500 transition duration-200"
-                            onClick={showPdf}
-                        >
-                            Afficher le PDF
-                        </button>
-                    </div>
+                    <button
+                        onClick={togglePDF}
+                        className={`mt-6 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 lg:hidden ${showPDF ? 'hidden' : 'block'}`}
+                    >
+                        {showPDF ? 'Fermer PDF' : 'Afficher PDF'}
+                    </button>
                 </div>
+
             </div>
 
-            {/* Modal pour le bouton Appliquer */}
-            {isModalOpen && (
-                <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-70">
-                    <div className="bg-white p-6 rounded shadow-lg">
-                        <h2 className="text-lg font-bold mb-4">Confirmation de la candidature</h2>
-                        <p>Êtes-vous sûr de vouloir appliquer pour le stage chez {entrepriseName} ?</p>
-                        <div className="flex justify-end mt-4">
+            {showPDF && (
+                <div className="fixed inset-0 bg-gray-100 z-50 flex justify-center items-center lg:hidden">
+                    <div className="w-full h-full">
+                        <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
+                            <Viewer
+                                fileUrl={pdfUrl}
+                                plugins={[defaultLayoutPluginInstance]}
+                                className="w-full h-full"
+                                style={{ padding: 0, minHeight: '100vh' }}
+                            />
                             <button
-                                className="bg-green-600 text-white font-bold py-2 px-4 rounded hover:bg-green-500 transition duration-200 mr-2"
-                                onClick={closeModal}
+                                onClick={togglePDF}
+                                className="absolute top-11 left-12 bg-red-400 text-white px-4 py-2 rounded hover:bg-red-500"
                             >
-                                Confirmer
+                                Fermer PDF
                             </button>
-                            <button
-                                className="bg-red-600 text-white font-bold py-2 px-4 rounded hover:bg-red-500 transition duration-200"
-                                onClick={closeModal}
-                            >
-                                Annuler
-                            </button>
-                        </div>
+                        </Worker>
                     </div>
                 </div>
             )}
@@ -129,3 +87,4 @@ const AppliquerStage = ({ vraiProps }) => {
 };
 
 export default AppliquerStage;
+
