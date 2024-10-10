f26140195ab195eb3a60c7cb7605b59781747044
JasonJody <jasonjody2005@gmail.com>
2024-10-10 03:56:59 -0400

    Fix variable pour affichage

--- front-end/src/components/listeOffreEmployeur/infoDetailleeOffreStage.jsx
+++ front-end/src/components/listeOffreEmployeur/infoDetailleeOffreStage.jsx
@@ -13,6 +13,14 @@ const InfoDetailleeOffreStage = ({setActiveOffer, activeOffer, getColorOffreStat
             return activeOffer.fileData ? "file" : "form";
     }
 
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const year = date.getFullYear();
+        return `${day}-${month}-${year}`;
+    }
+
     return (
         <div className={`pb-8 sm:pt-0 bg-orange-light z-20 rounded border border-deep-orange-200 w-full md:h-[calc(100vh-1rem)] h-[90vh] fixed left-0 md:sticky md:top-2 flex flex-col md:transition-none transition-all ease-in-out overflow-y-auto ${activeOffer === null ? "bottom-[-90vh]" : "bottom-0"}`}>
             <button className='absolute right-2 top-2 md:hidden'
@@ -22,8 +30,8 @@ const InfoDetailleeOffreStage = ({setActiveOffer, activeOffer, getColorOffreStat
             { activeOffer &&
                 <div className="p-6">
                     <h2 className="text-2xl font-bold mb-2">{activeOffer.title}</h2>
-                    <p className="text-sm text-gray-600">{t("postedOn")} : <span className="font-medium">{activeOffer.created_at}</span></p>
-                    <p className="text-sm text-gray-600">{t("updatedOn")} : <span className="font-medium">{activeOffer.updated_at}</span></p>
+                    <p className="text-sm text-gray-600">{t("postedOn")} : <span className="font-medium">{formatDate(activeOffer.createdAt)}</span></p>
+                    <p className="text-sm text-gray-600">{t("updatedOn")} : <span className="font-medium">{formatDate(activeOffer.updatedAt)}</span></p>
                     <h3 className="text-xl font-semibold mt-4">{t("offerDetails")} :</h3>
                     { activeOffer.description &&
                         <p className="mt-2"><strong>{t("description")} :</strong> {activeOffer.description}</p>
@@ -41,7 +49,7 @@ const InfoDetailleeOffreStage = ({setActiveOffer, activeOffer, getColorOffreStat
                     <p className="mt-2"><strong>{t("status")} :</strong> <span className={`font-semibold ${getColorOffreStatus(activeOffer.status)}`}>{t(activeOffer.status)}</span></p>
                     { activeOffer.website &&
                         <p className="mt-2">
-                            <strong>{t("websiteStage")} :</strong> <a href={activeOffer.website} target="_blank" className="text-blue-500 underline cursor-pointer">{activeOffer.website}</a>
+                            <strong>{t("websiteStage")} :</strong> <a href={"https://" + activeOffer.website} target="_blank" className="text-blue-500 underline cursor-pointer">{activeOffer.website}</a>
                         </p>
                     }
                     { format === "file" &&
