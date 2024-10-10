f941cd8e09602589f0d25b7ab271a08d0b006d98
Vicente Cabezas <1977259@claurendeau.qc.ca>
2024-10-09 21:00:38 -0400

    Ajout - test success & null EtudiantService methode getStages + methode private listeOffreStageToDTO

--- src/test/java/com/projet/mycose/service/EtudiantServiceTest.java
+++ src/test/java/com/projet/mycose/service/EtudiantServiceTest.java
@@ -1,11 +1,12 @@
 package com.projet.mycose.service;
 
-import com.projet.mycose.modele.Etudiant;
-import com.projet.mycose.modele.Programme;
+import com.projet.mycose.modele.*;
 import com.projet.mycose.modele.auth.Role;
 import com.projet.mycose.repository.EtudiantRepository;
+import com.projet.mycose.repository.OffreStageRepository;
 import com.projet.mycose.service.dto.EnseignantDTO;
 import com.projet.mycose.service.dto.EtudiantDTO;
+import com.projet.mycose.service.dto.OffreStageDTO;
 import jdk.jshell.execution.Util;
 import lombok.experimental.UtilityClass;
 import org.junit.jupiter.api.*;
@@ -15,13 +16,18 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class EtudiantServiceTest {
@@ -34,6 +40,9 @@ public class EtudiantServiceTest {
 
     @Mock
     private UtilisateurService utilisateurService;
+
+    @Mock
+    private OffreStageRepository offreStageRepositoryMock;
 
     @InjectMocks
     private EtudiantService etudiantService;
@@ -48,13 +57,13 @@ public class EtudiantServiceTest {
         EtudiantDTO etudiantDTO = etudiantService.creationDeCompte("Karim", "Mihoubi", "438-532-2729", "mihoubi@gmail.com", "Mimi123$", Programme.TECHNIQUE_INFORMATIQUE);
 
         //Assert
-        Assertions.assertEquals(etudiantDTO.getId(), 1);
-        Assertions.assertEquals(etudiantDTO.getPrenom(), "Karim");
-        Assertions.assertEquals(etudiantDTO.getNom(), "Mihoubi");
-        Assertions.assertEquals(etudiantDTO.getCourriel(), "mihoubi@gmail.com");
-        Assertions.assertEquals(etudiantDTO.getNumeroDeTelephone(), "438-532-2729");
-        Assertions.assertEquals(etudiantDTO.getProgramme(), Programme.TECHNIQUE_INFORMATIQUE);
-        Assertions.assertEquals(etudiantDTO.getRole(), Role.ETUDIANT);
+        assertEquals(etudiantDTO.getId(), 1);
+        assertEquals(etudiantDTO.getPrenom(), "Karim");
+        assertEquals(etudiantDTO.getNom(), "Mihoubi");
+        assertEquals(etudiantDTO.getCourriel(), "mihoubi@gmail.com");
+        assertEquals(etudiantDTO.getNumeroDeTelephone(), "438-532-2729");
+        assertEquals(etudiantDTO.getProgramme(), Programme.TECHNIQUE_INFORMATIQUE);
+        assertEquals(etudiantDTO.getRole(), Role.ETUDIANT);
     }
 
     @Test
@@ -144,6 +153,78 @@ public class EtudiantServiceTest {
         EtudiantDTO result = etudiantService.creationDeCompte("Karim", "Mihoubi", "450-691-0000", "mihoubi@gmail.com", "Mimi123$", Programme.TECHNIQUE_INFORMATIQUE);
 
         // Assert
-        Assertions.assertNull(result);
+        assertNull(result);
     }
+
+    @Test
+    public void testListeOffreStageToDTO() {
+        //Arrange
+        List<OffreStage> mockOffreStageList = new ArrayList<>();
+        FormulaireOffreStage mockFormulaireOffreStage = new FormulaireOffreStage("unTitre", "uneEntreprise", "unEmployeur", "unEmail@mail.com", "unsite.com", "uneLocalisation", "1000", "uneDescription", null);
+        FichierOffreStage mockFichierOffreStage = new FichierOffreStage("unTitre", "uneEntreprise", "nom.pdf", "data".getBytes(), null);
+
+        mockOffreStageList.add(mockFormulaireOffreStage);
+        mockOffreStageList.add(mockFichierOffreStage);
+
+        //Act
+        //List<OffreStageDTO> resultat = etudiantService.
+
+        //Assert
+    }
+
+    @Test
+    public void testGetStages_Success() {
+        // Arrange
+        String token = "unTokenValide";
+        Long etudiantId = 1L;
+        int page = 0;
+        PageRequest pageRequest = PageRequest.of(page, 10);
+        Employeur createur = new Employeur(2L, "unPrenom", "unNom", "514-222-0385", "courriel@courriel.com", "123123123", "uneEntreprise");
+
+        FormulaireOffreStage mockFormulaireOffreStage = new FormulaireOffreStage("unTitreForm", "uneEntreprise", "unEmployeur", "unEmail@mail.com", "unsite.com", "uneLocalisation", "1000", "uneDescription", createur);
+        FichierOffreStage mockFichierOffreStage = new FichierOffreStage("unTitreFichier", "uneEntreprise", "nom.pdf", "data".getBytes(), createur);
+        List<OffreStage> mockOffresListe = new ArrayList<>();
+        mockOffresListe.add(mockFormulaireOffreStage);
+        mockOffresListe.add(mockFichierOffreStage);
+
+        Page<OffreStage> offresPage = new PageImpl<>(mockOffresListe, pageRequest, 2);
+
+        when(utilisateurService.getUserIdByToken(token)).thenReturn(etudiantId);
+        when(offreStageRepositoryMock.findOffresByEtudiantId(etudiantId, pageRequest)).thenReturn(offresPage);
+
+        // Act
+        List<OffreStageDTO> result = etudiantService.getStages(token, page);
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals("unTitreForm", result.get(0).getTitle());
+        assertEquals("unTitreFichier", result.get(1).getTitle());
+
+        verify(utilisateurService, times(1)).getUserIdByToken(token);
+        verify(offreStageRepositoryMock, times(1)).findOffresByEtudiantId(etudiantId, pageRequest);
+    }
+
+    @Test
+    public void testGetStages_Null() {
+        // Arrange
+        String token = "unTokenValide";
+        Long etudiantId = 1L;
+        int page = 0;
+        PageRequest pageRequest = PageRequest.of(page, 10);
+        Page<OffreStage> offresPage = new PageImpl<>(List.of(), pageRequest, 0);
+
+        when(utilisateurService.getUserIdByToken(token)).thenReturn(etudiantId);
+        when(offreStageRepositoryMock.findOffresByEtudiantId(etudiantId, pageRequest)).thenReturn(offresPage);
+
+        // Act
+        List<OffreStageDTO> result = etudiantService.getStages(token, page);
+
+        // Assert
+        assertNull(result);
+
+        verify(utilisateurService, times(1)).getUserIdByToken(token);
+        verify(offreStageRepositoryMock, times(1)).findOffresByEtudiantId(etudiantId, pageRequest);
+    }
+
 }
