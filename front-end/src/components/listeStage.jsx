a411b262d75ba42672d849b1369809ce20887998
Samuel <samleveille8@gmail.com>
2024-10-10 03:54:26 -0400

    Ajout du Navigate dans listeStage

--- front-end/src/components/listeStage.jsx
+++ front-end/src/components/listeStage.jsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 import {useTranslation} from "react-i18next";
+import {useNavigate} from "react-router-dom";
 
 const listeStage = () => {
 
