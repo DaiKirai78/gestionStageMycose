07393d707bdf70661f2a3f05adf4dd3177912356
Vicente Cabezas <1977259@claurendeau.qc.ca>
2024-10-17 22:39:49 -0400

    Ajout - recuperer etudiants qui n'ont pas de profs assignees

--- src/main/java/com/projet/mycose/repository/UtilisateurRepository.java
+++ src/main/java/com/projet/mycose/repository/UtilisateurRepository.java
@@ -1,6 +1,10 @@
 package com.projet.mycose.repository;
 
+import com.projet.mycose.dto.EtudiantDTO;
+import com.projet.mycose.modele.Etudiant;
 import com.projet.mycose.modele.Utilisateur;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,4 +22,9 @@ public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> 
     Optional<Utilisateur> findUtilisateurByNumeroDeTelephone(String numero);
 
     Optional<Utilisateur> findUtilisateurById(Long id);
+
+    @Query("SELECT e FROM Etudiant e " +
+            "WHERE e.enseignantAssignee IS NULL " +
+            "AND e.credentials.role = 'ETUDIANT'")
+    Page<Etudiant> findAllEtudiantsSansEnseignants(Pageable pageable);
 }
