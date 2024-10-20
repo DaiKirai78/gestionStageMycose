4b6b2a027ff10c14510c52be3e1f33d82df97459
Vicente Cabezas <1977259@claurendeau.qc.ca>
2024-10-20 16:22:32 -0400

    Ajout - success test methode rechercherEnseignants dans GestionnaireStageController

--- src/test/java/com/projet/mycose/controller/GestionnaireStageControllerTest.java
+++ src/test/java/com/projet/mycose/controller/GestionnaireStageControllerTest.java
@@ -1,5 +1,6 @@
 package com.projet.mycose.controller;
 
+import com.projet.mycose.dto.EnseignantDTO;
 import com.projet.mycose.dto.EtudiantDTO;
 import com.projet.mycose.modele.Programme;
 import com.projet.mycose.modele.auth.Role;
@@ -81,5 +82,33 @@ public class GestionnaireStageControllerTest {
                         .param("pageNumber", String.valueOf(0))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void testRechercherEnseignants_Success() throws Exception {
+        // Arrange
+        EnseignantDTO enseignantDTOMock = new EnseignantDTO(
+                1L,
+                "Vicente",
+                "Cabezas",
+                "vicen@gmail.com",
+                "514-556-5566",
+                Role.ENSEIGNANT
+        );
+
+        List<EnseignantDTO> listeEnseignantsDTOMock = new ArrayList<>();
+        listeEnseignantsDTOMock.add(enseignantDTOMock);
+
+        when(gestionnaireStageService.getEnseignantsParRecherche("vicente")).thenReturn(listeEnseignantsDTOMock);
+
+        // Act & Assert
+        mockMvc.perform(post("/gestionnaire/rechercheEnseignants")
+                        .param("search", "vicente")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isAccepted())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].id").value(1L))
+                .andExpect(jsonPath("$[0].courriel").value("vicen@gmail.com"))
+                .andExpect(jsonPath("$[0].numeroDeTelephone").value("514-556-5566"));
     }
 }
