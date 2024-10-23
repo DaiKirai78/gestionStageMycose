f49ebfa138e35d395260ef2989c0208ad90fe778
Elie-X <eliebouchergendron@gmail.com>
2024-10-23 17:59:34 -0400

    Changement Controller OffreStage pour ModelAttribute avec FormData du front-end

--- src/main/java/com/projet/mycose/controller/OffreStageController.java
+++ src/main/java/com/projet/mycose/controller/OffreStageController.java
@@ -75,8 +75,8 @@ public class OffreStageController {
         return ResponseEntity.status(HttpStatus.OK).body(offreStageService.getTotalWaitingOffreStages());
     }
 
-    @PatchMapping("/accept")
-    public ResponseEntity<?> acceptOffreStage(@Valid @RequestBody AcceptOffreDeStageDTO acceptOffreDeStageDTO) {
+    @PatchMapping(value = "/accept", consumes = "multipart/form-data")
+    public ResponseEntity<?> acceptOffreStage(@Valid @ModelAttribute AcceptOffreDeStageDTO acceptOffreDeStageDTO) {
             offreStageService.acceptOffreDeStage(acceptOffreDeStageDTO);
             return ResponseEntity.ok().build();
     }
