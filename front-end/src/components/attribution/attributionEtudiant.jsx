f260222afc485bd1da44b24093e1e8c6dced2ed2
Zachary Girard <2291168@claurendeau.qc.ca>
2024-10-30 20:56:49 -0400

    changer l'affichage des programmes pour attribuer un étudiant

--- front-end/src/components/attribution/attributionEtudiant.jsx
+++ front-end/src/components/attribution/attributionEtudiant.jsx
@@ -8,11 +8,15 @@ import AssignCard from './assignCard';
 const AttributionEtudiant = () => {
     const { setSelectedStudent, programme, setProgramme } = useOutletContext();
     const [students, setStudents] = useState();
+    const [programmes, setProgrammes] = useState([]);
     const [isFetching, setIsFetching] = useState();
     const { t } = useTranslation();
     const [pages, setPages] = useState({minPages: 1, maxPages: null, currentPage: 1});
     const navigate = useNavigate();
 
+    useEffect(() => {
+        fetchProgrammes()
+    }, []);
 
     useEffect(() => {
         setPages({minPages: 1, maxPages: pages.maxPages === null ? undefined : null, currentPage: 1});
@@ -21,6 +25,20 @@ const AttributionEtudiant = () => {
     useEffect(() => {
         fetchAll()
     }, [pages.currentPage, pages.maxPages])
+
+    async function fetchProgrammes() {
+        try {
+            const response = await fetch("http://localhost:8080/api/programme");
+            if (response.ok) {
+                const data = await response.json();
+                setProgrammes(data);
+            } else {
+                console.error("Erreur lors de la récupération des programmes");
+            }
+        } catch (e) {
+            console.error("Erreur lors de la récupération des programmes " + e);
+        }
+    }
 
     async function fetchAll() {
         setIsFetching(true);
@@ -45,11 +63,11 @@ const AttributionEtudiant = () => {
                     method: "POST",
                     headers: {Authorization: `Bearer ${token}`}
                 }
-            );            
+            );
 
             if (response.ok) {
                 const data = await response.text();
-                
+
                 if (data) {
                     setStudents(JSON.parse(data));
                 } else {
@@ -60,7 +78,7 @@ const AttributionEtudiant = () => {
             console.error("Erreur lors de la récupération des élèves " + e);
         }
     }
-    
+
     async function fetchPages() {
         try {
             const token = localStorage.getItem("token");
@@ -73,7 +91,7 @@ const AttributionEtudiant = () => {
 
             if (response.ok) {
                 const data = await response.text();
-                
+
                 if (data) {
                     setPages((prev) => ({
                         ...prev,
@@ -96,31 +114,32 @@ const AttributionEtudiant = () => {
     return (
         <div className='w-full min-h-full bg-orange-light flex flex-col items-center p-6 gap-y-8'>
             <h1 className='text-3xl md:text-4xl font-bold text-center'>{t("attribuerProfEtudiant")}</h1>
-            <select 
+            <select
                 disabled={isFetching}
                 className='px-4 py-2 shadow rounded max-w-full'
-                name="programmeDropDown" 
-                id="programmeDropDown" 
-                value={programme} 
+                name="programmeDropDown"
+                id="programmeDropDown"
+                value={programme}
                 onChange={(e) => setProgramme(e.target.value)}>
-                <option value="NOT_SPECIFIED">{t("NOT_SPECIFIED")}</option>
-                <option value="TECHNIQUE_INFORMATIQUE">{t("TECHNIQUE_INFORMATIQUE")}</option>
-                <option value="GENIE_LOGICIEL">{t("GENIE_LOGICIEL")}</option>
-                <option value="RESEAU_TELECOMMUNICATION">{t("RESEAU_TELECOMMUNICATION")}</option>
+                {programmes.map((programme, index) => (
+                    <option key={index} value={programme}>
+                        {t(programme)}
+                    </option>
+                ))}
             </select>
             {isFetching ? <PageIsLoading />
-            :
-            <div>
-                <div className='flex flex-col gap-3 mb-5'>
-                    {
-                        students != null ? students.map((etudiant, index) => {
-                            return <AssignCard key={index} action={assignerProf} personne={etudiant} text={t("searchTeacher")}/>
-                        })
-                        : <p className='text-center'>{t("noStudents")}</p>
-                    }
+                :
+                <div>
+                    <div className='flex flex-col gap-3 mb-5'>
+                        {
+                            students != null ? students.map((etudiant, index) => {
+                                    return <AssignCard key={index} action={assignerProf} personne={etudiant} text={t("searchTeacher")}/>
+                                })
+                                : <p className='text-center'>{t("noStudents")}</p>
+                        }
+                    </div>
+                    {pages.maxPages ? <BoutonAvancerReculer pages={pages} setPages={setPages} /> : ""}
                 </div>
-                {pages.maxPages ? <BoutonAvancerReculer pages={pages} setPages={setPages} /> : ""}
-            </div>
             }
         </div>
     );
