9f550dffef09345c46f05482eca2a6eb80f2a861
JasonJody <jasonjody2005@gmail.com>
2024-11-05 19:03:12 -0500

    Ajout page signer contrat etudiant

--- front-end/src/pages/signerUnContratPage.jsx
+++ front-end/src/pages/signerUnContratPage.jsx
@@ -4,11 +4,11 @@ import SignerContrat from '../components/signerContrats/signerContrat';
 import TokenPageContainer from './tokenPageContainer';
 
 const SignerUnContratPage = () => {
-    const { setSelectedContract, setUserInfo, selectedContract } = useOutletContext();
+    const { setSelectedContract, setUserInfo, selectedContract, userInfo } = useOutletContext();
     
     return (
         <TokenPageContainer role={["ETUDIANT", "EMPLOYEUR", "GESTIONNAIRE_STAGE"]} setUserInfo={setUserInfo}>
-            <SignerContrat selectedContract={selectedContract} setSelectedContract={setSelectedContract} />
+            <SignerContrat selectedContract={selectedContract} setSelectedContract={setSelectedContract} userRole={userInfo.role} />
         </TokenPageContainer>
     );
 };
