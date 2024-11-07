40bcc8425efb575ff2d126fb775c7b0655164c2b
Zachary Girard <2291168@claurendeau.qc.ca>
2024-11-07 00:49:48 -0500

    active offer modifié pour afficher offres stages employeur

--- front-end/src/components/accueil/accueilEmployeur.jsx
+++ front-end/src/components/accueil/accueilEmployeur.jsx
@@ -58,7 +58,6 @@ const AccueilEmployeur = () => {
         try {
             let url = `${baseUrl}/getOffresPosted?pageNumber=${pages.currentPage - 1}`;
 
-            // Ajouter les paramètres `annee` et `session` si définis
             if (annee && session) {
                 url += `&annee=${annee}&session=${session}`;
             }
@@ -69,21 +68,25 @@ const AccueilEmployeur = () => {
             });
 
             if (response.status === STATUS_CODE_ACCEPTED) {
-                const text = await response.text(); // Lire la réponse en tant que texte
+                const text = await response.text();
                 if (text) {
-                    const fetchedData = JSON.parse(text); // Analyser en JSON si ce n'est pas vide
+                    const fetchedData = JSON.parse(text);
                     setData(fetchedData);
+
+                    if (fetchedData && fetchedData.length > 0) {
+                        setActiveOffer(fetchedData[0]);
+                    }
                 } else {
-                    setData(null); // Aucun contenu dans la réponse
+                    setData(null);
                     console.log("Nothing found");
                 }
             } else if (response.status === STATUS_CODE_NO_CONTENT) {
-                setData(null); // Aucun contenu, donc aucun résultat
+                setData(null);
                 console.log("Nothing found");
             }
         } catch (e) {
             console.log(e);
-            setData(null); // En cas d'erreur, définir `data` à null pour indiquer l'absence de résultats
+            setData(null);
         }
     }
 
@@ -94,7 +97,6 @@ const AccueilEmployeur = () => {
         try {
             let url = `${baseUrl}/pagesForCreateur`;
 
-            // Ajouter les paramètres `annee` et `session` si définis
             if (annee && session) {
                 url += `?annee=${annee}&session=${session}`;
             }
@@ -134,7 +136,6 @@ const AccueilEmployeur = () => {
         <TokenPageContainer role={["EMPLOYEUR"]} setUserInfo={setUserInfo}>
             <div className={`bg-orange-light w-full min-h-full flex-1 flex flex-col items-center gap-10 p-5`}>
                 <div className='w-4/5'>
-                    {/* Ajout du composant FiltreSession au-dessus de ListOffreStageEmployeur */}
                     <FiltreSession
                         annee={annee}
                         setAnnee={setAnnee}
