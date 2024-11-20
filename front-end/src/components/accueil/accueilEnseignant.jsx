9756d6fb565c291e81d53c777a3152aa6e45aac0
JasonJody <jasonjody2005@gmail.com>
2024-11-20 11:03:26 -0500

    Page afficher liste étudiant a évaluer

--- front-end/src/components/accueil/accueilEnseignant.jsx
+++ front-end/src/components/accueil/accueilEnseignant.jsx
@@ -1,30 +1,66 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import EvaluerListEtudiant from '../evaluer/evaluerListEtudiant';
 import PageTitle from '../pageTitle';
 import { useTranslation } from 'react-i18next';
+import PageIsLoading from '../pageIsLoading';
 
-const students = [
-    {
-        prenom: "Jason prof",
-        nom: "Jody",
-    },
-    {
-        prenom: "Vicente prof",
-        nom: "Cabezas",
-    },
-    {
-        prenom: "Roberto prof",
-        nom: "Berrios",
-    }
-]
-
-const AccueilEnseignant = ({ setSelectedStudent }) => {
+const AccueilEnseignant = ({ setSelectedStudent, userInfo }) => {
     const { t } = useTranslation();
+    const [students, setStudents] = useState();
+    const [isFetching, setIsFetching] = useState(true);
+
+    useEffect(() => {
+        fetchStudents();
+        setIsFetching(false);
+    }, [])
+
+    async function fetchStudents() {
+        setIsFetching(true);
+        
+        try {
+            
+            const token = localStorage.getItem("token");
+
+            const response = await fetch(
+                `http://localhost:8080/enseignant/getAllEtudiantsAEvaluer?enseignantId=${userInfo.id}`,
+                    {
+                        method: "GET",
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+            );
+
+            if (!response.ok) {
+                Error("Aucun étudiant trouvé")
+            }
+            
+            const data = await response.json();
+            setStudents(data);
+            
+        } catch (e) {
+            console.log("Une erreur est survenu lors de l'envoie du formulaire: " + e);
+        } finally {
+            setIsFetching(false);
+        }
+    }
+
+    if (isFetching) {
+        return (
+            <div className='flex flex-1 flex-col items-center justify-center bg-orange-light p-8'>
+                <PageIsLoading />
+            </div>
+        );
+    }
 
     return (
-        <div className='flex-1 flex flex-col bg-orange-light p-10'>
+        <div className='flex flex-1 flex-col items-center bg-orange-light p-8'>
             <PageTitle title={t("evaluerEtudiant")} />
-            <EvaluerListEtudiant students={students} setSelectedStudent={setSelectedStudent} destination="/ens/formulaire" />
+            {
+                students && students.length > 0 ? <EvaluerListEtudiant students={students} setSelectedStudent={setSelectedStudent} destination={"/ens/formulaire"} /> 
+                :
+                <p>{t("noStudentToEvaluate")}</p>
+            }
         </div>
     );
 };
