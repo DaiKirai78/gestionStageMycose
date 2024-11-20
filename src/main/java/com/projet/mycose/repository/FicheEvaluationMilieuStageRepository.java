0d1d2dd77580c7cf0263859c699fa335a51abf38
JasonJody <jasonjody2005@gmail.com>
2024-11-20 10:45:56 -0500

    Fix repo FicheEvaluationMilieuStageRepository

--- src/main/java/com/projet/mycose/repository/FicheEvaluationMilieuStageRepository.java
+++ src/main/java/com/projet/mycose/repository/FicheEvaluationMilieuStageRepository.java
@@ -1,17 +1,21 @@
 package com.projet.mycose.repository;
 
 import com.projet.mycose.modele.Etudiant;
+import com.projet.mycose.modele.FicheEvaluationMilieuStage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface FicheEvaluationMilieuStageRepository extends JpaRepository<FicheEvaluationMilieuStageRepository, Long> {
+public interface FicheEvaluationMilieuStageRepository extends JpaRepository<FicheEvaluationMilieuStage, Long> {
 
     @Query("SELECT e FROM Etudiant e " +
+            "JOIN e.contrat c " +
             "WHERE e.contractStatus = :contractStatus " +
             "AND e.enseignantAssignee.id = :enseignantId " +
-            "AND e.contrat NOT IN (SELECT f.contrat FROM FicheEvaluationMilieuStage f)")
+            "AND c NOT IN (" +
+            "    SELECT f.contrat FROM FicheEvaluationMilieuStage f" +
+            ")")
     Optional<List<Etudiant>> findAllEtudiantsNonEvaluesByProf(Long enseignantId, Etudiant.ContractStatus contractStatus);
 }
